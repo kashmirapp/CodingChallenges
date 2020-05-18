3eae2f0aa6cf6ddaedb72f1b5abd594ee6028b80
Kashmira Puranik <34950046+kashmirapp@users.noreply.github.com>
2020-05-18 15:26:54 -0400

    Add files via upload
     Find all numbers that occur more than once in an array of integers.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace findOne
+{
+    class MainClass
+    {
+        static int findSingle (int []ar, int ar_size)
+        {
+            int res = ar[0];
+            for (int i = 1; i < ar_size; i++)
+                res = res ^ ar[i];
+            return res;
+        }
+        public static void Main(string[] args)
+        {
+            int[] ar = { 2, 3, 5, 4, 5, 3, 4 };
+            int n = ar.Length;
+
+            Console.WriteLine("Element showing up only once is " + findSingle(ar, n) +  "");
+        }
+    }
+}
